1bddb3350790c0efc47f98d66b98e1f07fd68ca3
Roxane Le Tensorer <roxane.letensorer@gmail.com>
2021-11-20 18:32:17 +0100

    Creates different tabs

--- app/ui/start.py
+++ app/ui/start.py
@@ -6,17 +6,50 @@ from gi.repository import Gtk
 
 class MyWindow(Gtk.Window):
     def __init__(self):
-        super().__init__(title="Hello World")
+        super().__init__(title="May the force be with you")
+        
+        #Creates the notebook, ie the global page with tabs
+        self.notebook = Gtk.Notebook()
+        self.add(self.notebook)
 
-        self.button = Gtk.Button(label="Click Here")
-        self.button.connect("clicked", self.on_button_clicked)
-        self.add(self.button)
+        #Creates one box for each tab
+        self.arrow_box = Gtk.Box(spacing=6)
+        self.simple_vol = Gtk.Box(spacing = 6)
+        self.file_picker = Gtk.Box(spacing=6)
+        
+        #Creates three tabs, each containing the corresponding box
+        self.notebook.append_page(self.arrow_box, Gtk.Label(label="Contrôle"))
+        self.notebook.append_page(self.simple_vol, Gtk.Label(label="Plan de vol simple"))
+        self.notebook.append_page(self.file_picker, Gtk.Label(label="Plan de vol complexe"))
+        
+        #Populates the first tab: Controle with arrows       
+        self.up_arrow = Gtk.Button(label="Up")
+        self.arrow_box.pack_start(self.up_arrow, True, True, 0)
+        
+        self.right_arrow = Gtk.Button(label="Right")
+        self.arrow_box.pack_start(self.right_arrow, True, True, 0)
+        
+        self.down_arrow = Gtk.Button(label="Down")
+        self.arrow_box.pack_start(self.down_arrow, True, True, 0)
+        
+        self.left_arrow = Gtk.Button(label="Left")
+        self.arrow_box.pack_start(self.left_arrow, True, True, 0)
+        
+        #Populates the second tab: Simple vol plan
+        #TODO: choose origin and destination, then call Nicolas's function
+        
+        #Populates the third tab: Complex vol plan
+        #TODO: pick a file, then call Lilian's function
+        
+        
+        
 
     def on_button_clicked(self, widget):
-        print("Hello World")
+        self.button.set_label('Click again!')
 
 
 win = MyWindow()
+win.set_border_width(10)
 win.connect("destroy", Gtk.main_quit)
 win.show_all()
 Gtk.main()
