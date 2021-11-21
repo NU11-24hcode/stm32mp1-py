b705cd84f3bd5a4f491e8ed6c28069b6b418c699
Roxane Le Tensorer <roxane.letensorer@gmail.com>
2021-11-21 08:38:45 +0100

    lab

--- app/scripts/hello_world.py
+++ app/scripts/hello_world.py
@@ -11,6 +11,15 @@ class MyWindow(Gtk.Window):
         self.button = Gtk.Button(label="Click Here")
         self.button.connect("clicked", self.on_button_clicked)
         self.add(self.button)
+        
+        self.x1_adjustment = Gtk.Adjustment(0, 0, 45, 1, 10, 0)
+        self.x1_label = Gtk.Label("Valeur X de départ")
+        self.x1_axis = Gtk.SpinButton()
+        self.x1_axis.set_adjustment(self.x1_adjustment)
+        
+        self.add(self.x1_axis)
+        
+        print(self.x1_axis.get_value_as_int())
 
     def on_button_clicked(self, widget):
         print("Hello World")
